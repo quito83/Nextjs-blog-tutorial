b67e94bb556d719472d7b6ccf577cbb86731a120
Quito <quitoapps@gmail.com>
2023-09-14 18:35:51 +0000

    Final

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -3,6 +3,7 @@ import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 import { getSortedPostsData } from '../lib/posts';
+import Date from '../components/date';
 
 export default function Home({allPostsData}) {
   return (
@@ -22,12 +23,12 @@ export default function Home({allPostsData}) {
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
+             <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
